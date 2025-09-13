346e8c136f8787b41389dcff9ccb52e80a028332
prestontighe <me@prestontighe.com>
2025-09-12 23:40:56 -0500

    0.0.17 support local_host

--- index.js
+++ index.js
@@ -1,57 +1,64 @@
-const localtunnel = require('localtunnel')
-const _ = require('lodash')
+const localtunnel = require("localtunnel");
+const _ = require("lodash");
 
 class ServerlessLocaltunnel {
-  constructor (serverless) {
-    this.serverless = serverless
-    this.options = _.get(this.serverless, 'service.custom.localtunnel')
+  constructor(serverless) {
+    this.serverless = serverless;
+    this.options = _.get(this.serverless, "service.custom.localtunnel");
     // Parse tunnel options
-    if(Array.isArray(this.options)){
-      this.tunnelsOptions = this.options
-    }else{
-      this.tunnelsOptions = [{
-        port: _.get(this.options, 'port', 8080),
-        subdomain: _.get(this.options, 'subdomain')
-      }]
+    if (Array.isArray(this.options)) {
+      this.tunnelsOptions = this.options.map((opt) => ({
+        port: _.get(opt, "port", 8080),
+        subdomain: _.get(opt, "subdomain"),
+        local_host: _.get(opt, "local_host", "localhost"),
+      }));
+    } else {
+      this.tunnelsOptions = [
+        {
+          port: _.get(this.options, "port", 8080),
+          subdomain: _.get(this.options, "subdomain"),
+          local_host: _.get(this.options, "local_host", "localhost"),
+        },
+      ];
     }
     // Run tunnels after serverless-offline
     this.hooks = {
-      'before:offline:start:init': this.runServer.bind(this)
-    }
+      "before:offline:start:init": this.runServer.bind(this),
+    };
   }
-  runTunnel(port, subdomain) {
-    let tunnel
-    let serverRestarted = false
+  runTunnel(port, subdomain, local_host) {
+    let tunnel;
+    let serverRestarted = false;
     const errorHandler = (e) => {
-      this.serverless.cli.log('Localtunnel error - restarting in 15 seconds')
-      console.log(e)
-      if(serverRestarted) return
-      serverRestarted = true
-      tunnel.close()
-      setTimeout(this.runTunnel.bind(this, port, subdomain), 15000)
-    }
+      this.serverless.cli.log("Localtunnel error - restarting in 15 seconds");
+      console.log(e);
+      if (serverRestarted) return;
+      serverRestarted = true;
+      tunnel.close();
+      setTimeout(this.runTunnel.bind(this, port, subdomain, local_host), 15000);
+    };
     try {
-      tunnel = localtunnel(port, {subdomain}, (err, tunnel) => {
+      tunnel = localtunnel(port, { subdomain, local_host }, (err, tunnel) => {
         if (err) {
-          this.serverless.cli.log(`Localtunnel.me error: ${err.message}`)
-        }else{
-          this.serverless.cli.log(`Localtunnel.me started: ${tunnel.url}`)
+          this.serverless.cli.log(`Localtunnel.me error: ${err.message}`);
+        } else {
+          this.serverless.cli.log(`Localtunnel.me started: ${tunnel.url}`);
         }
-      })
-      tunnel.on('close', () => {
-        this.serverless.cli.log('Localtunnel closed')
-      })
-      tunnel.on('error', errorHandler)
+      });
+      tunnel.on("close", () => {
+        this.serverless.cli.log("Localtunnel closed");
+      });
+      tunnel.on("error", errorHandler);
     } catch (e) {
-      errorHandler(e)
+      errorHandler(e);
     }
-    return tunnel
+    return tunnel;
   }
   runServer() {
-    this.serverless.cli.log('Localtunnel running server')
-    return this.tunnelsOptions.map(({port, subdomain}) => {
-      return this.runTunnel(port, subdomain)
-    })
+    this.serverless.cli.log("Localtunnel running server");
+    return this.tunnelsOptions.map(({ port, subdomain, local_host }) => {
+      return this.runTunnel(port, subdomain, local_host);
+    });
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,51 +1,54 @@
-const ServerlessLocaltunnel = require('./index')
-const testHost = 'https://localtunnel.me'
-const testSubdomain = 'randomsubdomain1239873'
-const testPort = 2000
+const ServerlessLocaltunnel = require("./index");
+const testHost = "https://localtunnel.me";
+const testSubdomain = "randomsubdomain1239873";
+const testPort = 2000;
 
-let serverless
+let serverless;
 beforeEach(() => {
   serverless = {
     cli: {
-      log: jest.fn(string => console.log(string))
-    }
-  }
-})
-test('Flat single localtunnel', () => {
+      log: jest.fn((string) => console.log(string)),
+    },
+  };
+});
+test("Flat single localtunnel", () => {
   serverless.service = {
     custom: {
       localtunnel: {
         port: testPort,
-        subdomain: testSubdomain
-      }
-    }
-  }
-  const serverlessLocaltunnel = new ServerlessLocaltunnel(serverless)
-  const tunnels = serverlessLocaltunnel.runServer()
-  const tunnel = tunnels[0]
+        subdomain: testSubdomain,
+        local_host: "127.0.0.1",
+      },
+    },
+  };
+  const serverlessLocaltunnel = new ServerlessLocaltunnel(serverless);
+  const tunnels = serverlessLocaltunnel.runServer();
+  const tunnel = tunnels[0];
   expect(tunnels).toHaveLength(1);
   expect(serverless.cli.log.mock.calls.length).toBe(1);
   expect(tunnel._closed).toBe(false);
   expect(tunnel._opt.port).toBe(testPort);
   expect(tunnel._opt.subdomain).toBe(testSubdomain);
-  expect(tunnel._opt.host).toBe(testHost);
-  tunnel.close()
+  expect(tunnel._opt.host).toBe("127.0.0.1");
+  tunnel.close();
   expect(serverless.cli.log.mock.calls.length).toBe(2);
   expect(tunnel._closed).toBe(true);
 });
-test('Multi tunnel setup', () => {
+test("Multi tunnel setup", () => {
   serverless.service = {
     custom: {
-      localtunnel: [{
-        port: 2001,
-        subdomain: '078v25vc2502039v8y0'
-      }]
-    }
-  }
-  const serverlessLocaltunnel = new ServerlessLocaltunnel(serverless)
-  const tunnels = serverlessLocaltunnel.runServer()
+      localtunnel: [
+        {
+          port: 2001,
+          subdomain: "078v25vc2502039v8y0",
+        },
+      ],
+    },
+  };
+  const serverlessLocaltunnel = new ServerlessLocaltunnel(serverless);
+  const tunnels = serverlessLocaltunnel.runServer();
   expect(tunnels).toHaveLength(1);
-  tunnels.forEach(tunnel => {
-    tunnel.close()
-  })
+  tunnels.forEach((tunnel) => {
+    tunnel.close();
+  });
 });
